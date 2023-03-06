41010054276d45d6347462916984e5b6a6714ca6
yahya alloucha <yahyafullstack01@gmail.com>
2023-03-06 19:11:35 +0100

    taem

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -30,7 +30,7 @@ export default function Nav() {
           </a>
           <a
             style={{ color: color ? "white" : "black" }}
-            href="#Team"
+            href="/api/propolsals/shentu/16"
             className="links"
           >
             Team
